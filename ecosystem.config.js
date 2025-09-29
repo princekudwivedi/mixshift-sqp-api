03ece0a7ba5a067f36abaa11b359c67fb71abdff
Prince Kumar Dwivedi <prince@whiz-solutions.com>
2025-09-29 17:46:20 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,27 +1,27 @@
 module.exports = {
   apps : [
       {
-        name: "qb-poc-backend",
-        script: "/var/www/html/qb-backend/index.js",
+        name: "sqp-data-node-backend",
+        script: "/var/www/html/sqp-data-node-backend/src/server.js",
         watch: false,
-	error_file: '/var/www/html/qb-backend/pm2/error.log',
-        out_file: '/var/www/html/qb-backend/pm2/output.log',
-        log_file: '/var/www/html/qb-backend/pm2/combined.log',
-        ignore_watch: ["/var/www/html/qb-backend/log/*", "/var/www/html/qb-backend/node_modules/*", "/var/www/html/qb-backend/db/*"], 
+		error_file: '/var/www/html/sqp-data-node-backend/pm2/error.log',
+        out_file: '/var/www/html/sqp-data-node-backend/pm2/output.log',
+        log_file: '/var/www/html/sqp-data-node-backend/pm2/combined.log',
+        ignore_watch: ["/var/www/html/sqp-data-node-backend/log/*", "/var/www/html/sqp-data-node-backend/node_modules/*", "/var/www/html/sqp-data-node-backend/db/*"], 
         env: {
-          "PORT": 4112,
+          "PORT": 4212,
           "NODE_ENV": "local"
         },
         env_development: {
-          "PORT": 4112,
+          "PORT": 4212,
           "NODE_ENV": "development",
         },
         env_staging: {
-          "PORT": 4112,
+          "PORT": 4212,
           "NODE_ENV": "staging",
         },
         env_production: {
-          "PORT": 4112,
+          "PORT": 4212,
           "NODE_ENV": "production",
         }
       }
