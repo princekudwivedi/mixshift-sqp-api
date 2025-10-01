3b3b78514f24b5cf6e1960b87333a23e51979ac2
Prince Kumar Dwivedi <prince@whiz-solutions.com>
2025-10-01 10:48:47 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,19 @@ module.exports = {
       {
         name: "sqp-data-node-backend",
         script: "/var/www/html/sqp-data-node-backend/src/server.js",
-        watch: false,
+        watch: true,
 		error_file: '/var/www/html/sqp-data-node-backend/pm2/error.log',
         out_file: '/var/www/html/sqp-data-node-backend/pm2/output.log',
         log_file: '/var/www/html/sqp-data-node-backend/pm2/combined.log',
-        ignore_watch: ["/var/www/html/sqp-data-node-backend/log/*", "/var/www/html/sqp-data-node-backend/node_modules/*", "/var/www/html/sqp-data-node-backend/db/*"], 
+        ignore_watch: [
+			"/var/www/html/sqp-data-node-backend/logs/*", 
+			"/var/www/html/sqp-data-node-backend/node_modules/*", 
+			"/var/www/html/sqp-data-node-backend/db/*",
+			"/var/www/html/sqp-data-node-backend/pm2/*"
+		], 
+		exec_mode: "cluster",
+		instances: 2,
+		env_file: ".env",
         env: {
           "PORT": 4212,
           "NODE_ENV": "local"
